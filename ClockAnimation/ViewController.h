f03c816e9a16216abc73368f6f7f097ea3a56476
Duncan Champney <dchampney@wareto.com>
2014-02-06 11:08:33 -0500

    Initial Commit

---
+++ ClockAnimation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ClockAnimation
+//
+//  Created by Duncan Champney on 2/6/14.
+//  Copyright (c) 2014 WareTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
